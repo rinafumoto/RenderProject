44711ea2feea62d2dfc3e320a44c5e33c59548ef
rf1112 <rina.fumoto@icloud.com>
2022-03-11 20:53:16 +0000

    Added math for more accurate modelling

--- perfume.py
+++ perfume.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import prman
+import math
 
 ri = prman.Ri()
 
@@ -16,43 +17,63 @@ ri.Projection(ri.PERSPECTIVE,{ri.FOV:100})
 ri.WorldBegin()
 # ri.Scale(0.1,0.1,0.1)
 ri.Translate(0, 0, 10)
-# ri.Rotate(-25, 1, 0, 0)
+# ri.Rotate(-65, 1, 0, 0)
+
+bodyHeight = 6.7
+bodyRadius = 1.65
+capHeight = 2.8
+capRadius = 0.95
+middleHeight = 1.3
 
 ### Body ###
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
-ri.Cylinder(1.65, -6.7, 0, 360)
-ri.Translate(0, 0, -0.05)
-ri.Sphere(1.650757402, 0.05, 1.35, 360)
+ri.Cylinder(bodyRadius, -bodyHeight, 0, 360)
 ri.TransformEnd()
 
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
-ri.Translate(0, 0, -6.7)
-ri.Torus(1.35, 0.3, -135, 0,360)
+a = bodyRadius
+b = capRadius
+h = middleHeight
+d = (a*a-b*b-h*h)/(2*h)
+r = math.sqrt(((a-b)*(a-b)+h*h)*((a+b)*(a+b)+h*h)/(4*h*h))
+ri.Translate(0, 0, -d)
+ri.Sphere(r, d, h+d, 360)
 ri.TransformEnd()
 
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
-ri.Disk(-6.92, 1.14, 360)
+ri.Translate(0, 0, -bodyHeight)
+minorRadius = 0.3
+degree = -135
+ri.Torus(bodyRadius-minorRadius, minorRadius, degree, 0, 360)
+ri.TransformEnd()
+
+ri.TransformBegin()
+ri.Rotate(-90, 1, 0, 0)
+height = -bodyHeight+math.sin(math.radians(degree))*minorRadius
+radius = bodyRadius-minorRadius+math.sin(math.radians(degree))*minorRadius
+ri.Disk(height, radius, 360)
 ri.TransformEnd()
 
 ### Cap ###
 ri.TransformBegin()
-ri.Translate(0, 1.3, 0)
+ri.Translate(0, middleHeight, 0)
 ri.Rotate(-90, 1, 0, 0)
-ri.Cylinder(0.95, 0, 2.8, 360)
+ri.Cylinder(capRadius, 0, capHeight, 360)
 ri.TransformEnd()
 
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
-ri.Translate(0, 0, 4.1)
-ri.Torus(0.9, 0.05, 0, 90, 360)
+ri.Translate(0, 0, middleHeight+capHeight)
+minorRadius = 0.05
+ri.Torus(capRadius-minorRadius, minorRadius, 0, 90, 360)
 ri.TransformEnd()
 
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
-ri.Disk(4.15, 0.9, 360)
+ri.Disk(middleHeight+capHeight+minorRadius, capRadius-minorRadius, 360)
 ri.TransformEnd()
 
 ri.WorldEnd()
