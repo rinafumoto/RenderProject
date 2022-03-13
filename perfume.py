6767eaec0117cf3a4783aff9aea39d850ace6838
rf1112 <rina.fumoto@icloud.com>
2022-03-13 21:34:06 +0000

    Env map, material, space between cap and bottle

--- perfume.py
+++ perfume.py
@@ -4,20 +4,56 @@ import prman
 import math
 
 ri = prman.Ri()
-
 filename = "__render"  # "perfume.rib"
+
+# for i in range(12):
+    # deg = i*30
+
+##### SETTINGS #####
+
 ri.Begin(filename)
 
+# ri.Display("perfume_"+str(deg)+".exr", "it", "rgba")
 ri.Display("perfume.exr", "it", "rgba")
 ri.Format(720, 576, 1)
 # ri.Format(1920, 1080, 1)
+
+ri.Hider("raytrace", {"int incremental": [1]})
+# ri.ShadingRate(10)
+# ri.PixelVariance(0.1)
+ri.Integrator("PxrPathTracer", "integrator")
+
 # ri.Projection(ri.ORTHOGRAPHIC)
-ri.Projection(ri.PERSPECTIVE,{ri.FOV:100})
+ri.Projection(ri.PERSPECTIVE,{ri.FOV:40})
+
+##### GLOBAL TRANSFORMATION #####
+
+# ri.Scale(0.1,0.1,0.1)
+# ri.Translate(0, 1, 15)
+ri.Translate(0, 1.5, 20)
+# ri.Translate(0, 100, 0)
+ri.Rotate(-30, 1, 0, 0)
+# ri.Rotate(-5, 0, 0, 1)
 
 ri.WorldBegin()
-# ri.Scale(0.1,0.1,0.1)
-ri.Translate(0, 0, 10)
-# ri.Rotate(-65, 1, 0, 0)
+
+##### LIGHTING #####
+
+ri.TransformBegin()
+ri.AttributeBegin()
+ri.Declare("domeLight", "string")
+ri.Rotate(-90, 1, 0, 0)
+# ri.Rotate(deg,0,0,1)
+ri.Rotate(-30,0,0,1)
+ri.Light("PxrDomeLight", "domeLight", {
+    "string lightColorMap": "env/photo_studio_london_hall_8k.tx",
+    "float intensity": [0.8]
+    })
+
+ri.AttributeEnd()
+ri.TransformEnd()
+
+##### MODELLING #####
 
 bodyHeight = 6.7
 bodyRadius = 1.65
@@ -26,6 +62,14 @@ capRadius = 0.95
 middleHeight = 1.3
 
 ### Body ###
+
+ri.Bxdf("PxrDisney", "bxdf",{
+    "color baseColor" : [ 0.65, 0.27, 0.07 ],
+    "float metallic": [0.9],
+    "float anisotropic" : [0.6],
+    "float roughness": [0.3]
+    })
+
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
 ri.Cylinder(bodyRadius, -bodyHeight, 0, 360)
@@ -39,25 +83,34 @@ h = middleHeight
 d = (a*a-b*b-h*h)/(2*h)
 r = math.sqrt(((a-b)*(a-b)+h*h)*((a+b)*(a+b)+h*h)/(4*h*h))
 ri.Translate(0, 0, -d)
-ri.Sphere(r, d, h+d, 360)
+ri.Sphere(r, d, r, 360)
 ri.TransformEnd()
 
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
 ri.Translate(0, 0, -bodyHeight)
-minorRadius = 0.3
+bottomMinorRadius = 0.3
 degree = -135
-ri.Torus(bodyRadius-minorRadius, minorRadius, degree, 0, 360)
+ri.Torus(bodyRadius-bottomMinorRadius, bottomMinorRadius, degree, 0, 360)
 ri.TransformEnd()
 
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
-height = -bodyHeight+math.sin(math.radians(degree))*minorRadius
-radius = bodyRadius-minorRadius+math.sin(math.radians(degree))*minorRadius
+height = -bodyHeight+math.sin(math.radians(degree))*bottomMinorRadius
+radius = bodyRadius-bottomMinorRadius+math.sin(math.radians(degree))*bottomMinorRadius
 ri.Disk(height, radius, 360)
 ri.TransformEnd()
 
 ### Cap ###
+ri.TransformBegin()
+ri.Translate(0, 0.05, 0)
+
+ri.Bxdf("PxrDisney", "bxdf",{
+    "color baseColor" : [ 0.65, 0.27, 0.07 ],
+    "float metallic": [1],
+    "float roughness": [0]
+    })
+
 ri.TransformBegin()
 ri.Translate(0, middleHeight, 0)
 ri.Rotate(-90, 1, 0, 0)
@@ -74,6 +127,41 @@ ri.TransformEnd()
 ri.TransformBegin()
 ri.Rotate(-90, 1, 0, 0)
 ri.Disk(middleHeight+capHeight+minorRadius, capRadius-minorRadius, 360)
+ri.TransformEnd()
+ri.TransformEnd()
+
+### Table ###
+ri.Bxdf("PxrDisney", "bxdf",{
+    "color baseColor" : [ 1, 1, 1],
+    "float metallic": [0],
+    "float specular":[0],
+    "float roughness":[0.5]
+    })
+
+# expr = """
+# $colour = c1;
+# $c = floor( 10 * $u ) +floor( 10 * $v );
+# if( fmod( $c, 2.0 ) < 1.0 )
+# {
+# 	$colour=c2;
+# }
+# $colour
+# """
+
+# ri.Pattern("PxrSeExpr", "seTexture", {"color c1": [1, 1, 1], "color c2": [0, 0, 0], "string expression": [expr]})
+# ri.Bxdf(
+#     "PxrDiffuse",
+#     "diffuse",
+#     {
+#         #  'color diffuseColor'  : [1,0,0]
+#         "reference color diffuseColor": ["seTexture:resultRGB"]
+#     },
+# )
+
+ri.TransformBegin()
+ri.Rotate(-90, 1, 0, 0)
+ri.Translate(0, 0, -bodyHeight-bottomMinorRadius)
+# ri.Patch("bilinear", {"P": [-40,-80,0, 40,-80,0, -40,20,0, 40,20,0]})
 ri.TransformEnd()
 
 ri.WorldEnd()
