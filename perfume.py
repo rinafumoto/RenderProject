7c123281e4a47c46e76d1abfe94eb375a1f2d146
rf1112 <rina.fumoto@icloud.com>
2022-04-09 19:08:21 +0100

    Variation Done

--- perfume.py
+++ perfume.py
@@ -13,7 +13,8 @@ filename = "__render"  # "perfume.rib"
 
 ri.Begin(filename)
 # ri.Display("perfume_"+str(deg)+".exr", "it", "rgba")
-ri.Display("perfume.exr", "it", "rgba")
+ri.Display("render/perfume.exr", "file", "rgba")
+# ri.Display("perfume.exr", "it", "rgba")
 # ri.Format(720, 576, 1)
 ri.Format(1920, 1080, 1)
 
@@ -30,14 +31,9 @@ ri.Projection(ri.PERSPECTIVE,{ri.FOV:40})
 
 ##### GLOBAL TRANSFORMATION #####
 
-# ri.Scale(0.1,0.1,0.1)
-# ri.Translate(0, 1, 15)
-# ri.Translate(0, 1.5, 3)
+# ri.Translate(0, 2.5, 3.5)
 ri.Translate(0, 1.5, 20)
-# ri.Translate(0, 100, 0)
 ri.Rotate(-30, 1, 0, 0)
-# ri.Rotate(-5, 0, 0, 1)
-# ri.DepthOfField(5.6, 0.9, 20)
 
 ri.WorldBegin()
 
@@ -73,21 +69,21 @@ ri.Pattern("shader", "shader",{
 })
 
 ri.Attribute("displacementbound", {
-    "float sphere" : [0.01]
+    "float sphere" : [0.001]
 })
 
 ri.Pattern("PxrFlakes", "flakes", {
 	'float flakeAmount' : [0.05], 
 	'float flakeFreq' : [80], 
 	'float size' : [0.8], 
-	'int octaves' : [3], 
+	'int octaves' : [2], 
 })
 
 ri.AttributeBegin()
 
 ri.Displace("PxrDisplace", "disp", {
-    "float dispAmount": [-0.001],
-    "reference float dispScalar": ["shader:disp"]
+    "float dispAmount": [-0.0001],
+    "reference float dispScalar": ["shader:disp2"]
 })
 
 ri.Bxdf("PxrDisney", "bxdf",{
@@ -103,11 +99,12 @@ ri.Rotate(-90, 1, 0, 0)
 ri.Cylinder(bodyRadius, -bodyHeight, 0, 360)
 ri.TransformEnd()
 ri.AttributeEnd()
+
 ri.AttributeBegin()
 
 ri.Displace("PxrDisplace", "disp", {
-    "float dispAmount": [-0.001],
-    "reference float dispScalar": ["shader:resultF"]
+    "float dispAmount": [-0.0001],
+    "reference float dispScalar": ["shader:disp"]
 })
 
 ri.Bxdf("PxrDisney", "bxdf",{
@@ -146,79 +143,79 @@ ri.TransformEnd()
 
 ri.AttributeEnd()
 
-# ### Cap ###
+### Cap ###
 
-# ri.AttributeBegin()
+ri.AttributeBegin()
 
-# ri.TransformBegin()
-# ri.Translate(0, 0.05, 0)
+ri.TransformBegin()
+ri.Translate(0, 0.05, 0)
 
-# ri.Bxdf("PxrDisney", "bxdf",{
-#     "color baseColor" : [ 0.65, 0.27, 0.07 ],
-#     "float metallic": [1],
-#     "float roughness": [0]
-# })
+ri.Bxdf("PxrDisney", "bxdf",{
+    "color baseColor" : [ 0.65, 0.27, 0.07 ],
+    "float metallic": [1],
+    "float roughness": [0]
+})
 
-# ri.TransformBegin()
-# ri.Translate(0, middleHeight, 0)
-# ri.Rotate(-90, 1, 0, 0)
-# ri.Cylinder(capRadius, 0, capHeight, 360)
-# ri.TransformEnd()
+ri.TransformBegin()
+ri.Translate(0, middleHeight, 0)
+ri.Rotate(-90, 1, 0, 0)
+ri.Cylinder(capRadius, 0, capHeight, 360)
+ri.TransformEnd()
 
-# ri.TransformBegin()
-# ri.Rotate(-90, 1, 0, 0)
-# ri.Translate(0, 0, middleHeight+capHeight)
-# minorRadius = 0.05
-# ri.Torus(capRadius-minorRadius, minorRadius, 0, 90, 360)
-# ri.TransformEnd()
+ri.TransformBegin()
+ri.Rotate(-90, 1, 0, 0)
+ri.Translate(0, 0, middleHeight+capHeight)
+minorRadius = 0.05
+ri.Torus(capRadius-minorRadius, minorRadius, 0, 90, 360)
+ri.TransformEnd()
 
-# ri.TransformBegin()
-# ri.Rotate(-90, 1, 0, 0)
-# ri.Disk(middleHeight+capHeight+minorRadius, capRadius-minorRadius, 360)
-# ri.TransformEnd()
-# ri.TransformEnd()
+ri.TransformBegin()
+ri.Rotate(-90, 1, 0, 0)
+ri.Disk(middleHeight+capHeight+minorRadius, capRadius-minorRadius, 360)
+ri.TransformEnd()
+ri.TransformEnd()
 
-# ri.AttributeEnd()
+ri.AttributeEnd()
 
-# ### Table ###
+### Table ###
 
-# ri.AttributeBegin()
+ri.AttributeBegin()
 
-# ri.Pattern("PxrTexture", "myTexture", {
-#     "string filename" : ["textures/White_oak_pxr128.tx"]
-# })
-# ri.Bxdf("PxrDisney", "bxdf",{
-#     "reference color baseColor" : ["myTexture:resultRGB"]
-# })
+ri.Pattern("PxrTexture", "myTexture", {
+    "string filename" : ["textures/White_oak_pxr128.tx"]
+})
+ri.Bxdf("PxrDisney", "bxdf",{
+    "reference color baseColor" : ["myTexture:resultRGB"]
+})
 
-# # expr = """
-# # $colour = c1;
-# # $c = floor( 10 * $u ) +floor( 10 * $v );
-# # if( fmod( $c, 2.0 ) < 1.0 )
-# # {
-# # 	$colour=c2;
-# # }
-# # $colour
-# # """
+# expr = """
+# $colour = c1;
+# $c = floor( 10 * $u ) +floor( 10 * $v );
+# if( fmod( $c, 2.0 ) < 1.0 )
+# {
+# 	$colour=c2;
+# }
+# $colour
+# """
 
-# # ri.Pattern("PxrSeExpr", "seTexture", {"color c1": [1, 1, 1], "color c2": [0, 0, 0], "string expression": [expr]})
-# # ri.Bxdf(
-# #     "PxrDiffuse",
-# #     "diffuse",
-# #     {
-# #         #  'color diffuseColor'  : [1,0,0]
-# #         "reference color diffuseColor": ["seTexture:resultRGB"]
-# #     },
-# # )
+# ri.Pattern("PxrSeExpr", "seTexture", {"color c1": [1, 1, 1], "color c2": [0, 0, 0], "string expression": [expr]})
+# ri.Bxdf(
+#     "PxrDiffuse",
+#     "diffuse",
+#     {
+#         #  'color diffuseColor'  : [1,0,0]
+#         "reference color diffuseColor": ["seTexture:resultRGB"]
+#     },
+# )
 
-# ri.TransformBegin()
-# ri.Rotate(-90, 1, 0, 0)
-# ri.Rotate(90,0,0,1)
-# ri.Translate(-35, 0, -(bodyHeight+bottomMinorRadius))
-# ri.Patch("bilinear", {"P": [-40,-40,0, 40,-40,0, -40,40,0, 40,40,0]})
-# ri.TransformEnd()
+ri.TransformBegin()
+ri.Rotate(-90, 1, 0, 0)
+ri.Rotate(90,0,0,1)
+ri.Translate(-35, 0, -(bodyHeight+bottomMinorRadius))
+ri.Patch("bilinear", {"P": [-40,-40,0, 40,-40,0, -40,40,0, 40,40,0]})
+ri.TransformEnd()
 
-# ri.AttributeEnd()
+ri.AttributeEnd()
 
 ri.WorldEnd()
 ri.End()
